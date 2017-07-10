762906cb66c969d399d6b7ac527408bbc3567f8f
Luan Reis <luanReis@users.noreply.github.com>
2017-07-10 21:22:36 +0200

    Show touched number in the calculator display

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,8 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var display: UILabel?
+
+    var userIsInMiddleOfTyping = false
+
     @IBAction func touchDigit(_ sender: UIButton) {
         let digit = sender.currentTitle!
-        print("\(digit) was touched!")
+
+        if userIsInMiddleOfTyping {
+            let textCurrentlyInDisplay = display!.text!
+            display!.text = textCurrentlyInDisplay + digit
+        } else {
+            display!.text = digit
+            userIsInMiddleOfTyping = true
+        }
+
     }
 }
